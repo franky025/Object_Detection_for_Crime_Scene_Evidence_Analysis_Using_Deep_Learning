6d035aa0210adcfef6c5e9d68846a0b025d6351b
Pranay <pranay.franky@gmail.com>
2020-11-27 02:51:47 +0530

    Initial commit

---
+++ Object Detection for Crime Scene Evidence Analysis Using Deep Learning/crime.py
@@ -0,0 +1,72 @@
+import cv2 as cv
+import numpy as np
+
+#cap = cv.VideoCapture(0 + cv.CAP_DSHOW)
+cap = cv.VideoCapture('Input_Video/image_to_video.avi')
+
+
+whT = 320
+confThreshold = 0.50
+nmsThreshold = 0.2
+
+
+classesFile = "Crime/crime.names"
+classNames = []
+with open(classesFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+modelConfiguration = "Crime/crime.cfg"
+modelWeights = "Crime/crime.weights"
+
+
+
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+
+def findObjects(outputs, img):
+    hT, wT, cT = img.shape
+    bbox = []
+    classIds = []
+    confs = []
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w, h = int(det[2] * wT), int(det[3] * hT)
+                x, y = int((det[0] * wT) - w / 2), int((det[1] * hT) - h / 2)
+                bbox.append([x, y, w, h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+
+    indices = cv.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+
+    for i in indices:
+        i = i[0]
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        # print(x,y,w,h)
+        cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+        cv.putText(img, f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',
+                   (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+
+
+while True:
+    success, img = cap.read()
+    print(img.shape)
+    blob = cv.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+    net.setInput(blob)
+    layersNames = net.getLayerNames()
+    outputNames = [(layersNames[i[0] - 1]) for i in net.getUnconnectedOutLayers()]
+    outputs = net.forward(outputNames)
+    findObjects(outputs, img)
+    imgg = cv.resize(img, (960, 540))
+    cv.imshow('Image', imgg)
+    if cv.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
